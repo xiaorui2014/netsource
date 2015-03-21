e2da8a3a269c3a2868c38f15ff127dd02240db68
xiaorui <xiaorui2014@yeah.net>
2015-03-21 08:28:13 +0800

    20150320当前页查询功能已实现
    当前页查询功能，回车提交表单，优化代码，功能文件分离均已实现。问题：phpmyadmin导入excel乱码问题和导入数量少仍然没有解决。

--- index.php
+++ index.php
@@ -1,159 +1,26 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
 	<title>基站数据查询</title>
-	<link href="./css/jcsjys.css" rel="stylesheet">
+	<!--<link href="./css/jcsjys.css" rel="stylesheet">-->
 
-<script type="text/javascript">
-
-var xmlHttp
-
-function showCustomer(str)
-{ 
-xmlHttp=GetXmlHttpObject();
-if (xmlHttp==null)
-  {
-  alert ("您的浏览器不支持AJAX！");
-  return;
-  } 
-var url="search.php";
-url=url+"?q="+str;
-url=url+"&sid="+Math.random();
-xmlHttp.onreadystatechange=stateChanged;
-xmlHttp.open("GET",url,true);
-xmlHttp.send(null);
-}
-
-function stateChanged()
-{ 
-if (xmlHttp.readyState==4)
-{ 
-document.getElementById("txtHint").innerHTML=xmlHttp.responseText;
-}
-}
-
-function GetXmlHttpObject()
-{
-var xmlHttp=null;
-try
-  {
-  // Firefox, Opera 8.0+, Safari
-  xmlHttp=new XMLHttpRequest();
-  }
-catch (e)
-  {
-  // Internet Explorer
-  try
-    {
-    xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
-    }
-  catch (e)
-    {
-    xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-  }
-return xmlHttp;
-}
-</script>
+<script src="xianshi.js"></script>
 
 </head>
 <body>
 
-<div id="container">
-<div id="header">
-	<h1>基站数据查询</h1>
+<h1>基站数据查询</h1>
 
-</div>
-<!--<div id="menu">
-	<h2>menu1</h2>
-	<ul>
-		<li>menu2</li>
-		<li>menu3</li>
-		<li>menu4</li>
-	</ul>
-
-</div>-->
-<div id="search">
-<form>
-<input onchange="showCustomer(this.value)" type="text" size="15" />
-<!--<button type="button" onclick="loadXMLDoc()">查询</button>-->
+<form name="chaxun">
+<input onchange="showCustomer(this.value)" type="text" size="15" onkeydown="keyDown();" />
+<input type="button" id="btn" value="查找">
 
 </form>
-</div>
-<div id="content">
-<!--带搜索框的数据展示-->
 
-<h3>查询结果</h3>
-<div id="txtHint"></div>
-
-</div>
-<div id="footer"></div>
-</div>
-
-
-
-
-
-
-<!-- 直接查看全部数据，无搜索框，只能显示1000行以下。
-<?php
-    $link = mysql_connect('localhost','admin','admin');
-
-    mysql_query("SET NAMES 'UTF8'");
-
-    if(!$link){
-
-    	die('链接数据库失败，错误代码：'.mysql_errno());
-    }
-
-    mysql_select_db('jichushuju', $link);
-    $sql="SELECT xiaoquming,daiwei,jizhanming from jizhanbiao";
-    $result = mysql_query($sql,$link);
-    $table="<table>";
-    $table.='<td>小区名称</td>';
-    $table.='<td>代维公司</td>';
-    $table.='<td>基站名称</td>';
-    if($result&&mysql_num_rows($result)>0){
-
-    	while($row=mysql_fetch_assoc($result)){
-
-    		$table.='<tr>';
-    		$table.='<td align="center">'.$row['xiaoquming'].'</td>';
-    		$table.='<td align="center">'.$row['daiwei'].'</td>';
-    		$table.='<td align="center">'.$row['jizhanming'].'</td>';
-    		$table.="</tr>";
-    	}
-    }
-
-    else {
-
-    	$table.= '<tr><td colspan="3" align="center">没有找到数据</td></tr>';
-    }
-    $table.='</table>';
-
-    echo $table;
-
-
-
-?> -->
- 
- <!-- <table>
-      <tr>
-          <td>小区名称</td>
-          <td>代维公司</td>
-          <td>基站名称</td>
-      </tr>    先把标题写好 
-          <?php// while ($row =// mysql_fetch_assoc($result)) ?>
-          <tr>
-              <td><?php// echo $row["xiaoquming"] ?></td>
-              <td><?php// echo $row["daiwei"] ?></td>
-              <td><?php// echo $row["jizhanming"] ?></td>
-           </tr>
-           <?php// endwhile;    //数据操作完，结束循环
-      //  mysql_close($link); ?>    
-  </table> -->
+</br></br>
+<b>查询结果</b>
+<p><div id="txtHint"></div></p>
 
 </body>
 </html>
--- search.php
+++ search.php
@@ -1,42 +1,30 @@
 <?php
-
-$con = mysql_connect('localhost','admin','admin');
-mysql_query("SET NAMES 'UTF8'");
- if(!$con){
-
-        die('链接数据库失败，错误代码：'.mysql_errno());
-    }
-mysql_select_db('jichushuju', $con);
+require_once ('conn.php');
 
 
-            //$searchs=$_GET['q'];
-      //$searchs=trim($searchs);
-$sql="SELECT * from jizhanbiao where xiaoquming=".$_GET['q'];
-$result = mysql_query($sql,$conn);
-mysql_close($con);
+$searchs=$_GET['q'];
+$searchs=trim($searchs);
+$q=$_GET["q"];
+$sql="SELECT * from jizhanbiao where xiaoquming like '%$searchs%'";
+$result = mysql_query($sql);
 
-    echo '</br>';
-    echo '</br>';
-    echo '</br>';
-    echo '</br>';
+echo "<table border='1'>
+<tr>
+<th>小区名称</th>
+<th>代维公司</th>
+<th>基站名称</th>
+</tr>";
+   
 
-    $table="<table>";
-    $table.='<td>小区名称</td>';
-    $table.='<td>代维公司</td>';
-    $table.='<td>基站名称</td>';
-    if (mysql_num_rows($result) < 1) echo '没有数据';
+while($row=mysql_fetch_array($result)){
 
-    	while($row=mysql_fetch_array($result)){
-
-    		$table.='<tr>';
-    		$table.='<td align="center">'.$row['xiaoquming'].'</td>';
-    		$table.='<td align="center">'.$row['daiwei'].'</td>';
-    		$table.='<td align="center">'.$row['jizhanming'].'</td>';
-    		$table.="</tr>";
+echo "<tr>";
+echo "<td>" . $row['xiaoquming'] . "</td>";
+echo "<td>" . $row['daiwei'] . "</td>";
+echo "<td>" . $row['jizhanming'] . "</td>";
+echo "</tr>";
     	}
 
-
-    echo $table;
+echo "</table>";
    
-    
 ?>
